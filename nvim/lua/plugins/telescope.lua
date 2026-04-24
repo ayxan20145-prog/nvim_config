2b60dda1ade00acf9643ccf24bb67e99d12153bd
ayxan20145 <ayxan.20145@gmail.com>
2026-04-24 19:08:31 +0400

    nvim4

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,10 +1,10 @@
-return{
+{
   "nvim-telescope/telescope.nvim",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope-fzf-native.nvim",
   },
-  build = "make", -- builds the fast sorter (optional but recommended)
+  build = "make",
 
   config = function()
     local telescope = require("telescope")
@@ -17,22 +17,40 @@ return{
         path_display = { "smart" },
 
         layout_config = {
-          horizontal = { preview_width = 0.6 },
+          horizontal = {
+            preview_width = 0.6,
+          },
         },
 
         sorting_strategy = "ascending",
       },
     })
 
-    -- enable fzf (faster sorting)
+    -- load fzf extension (faster sorting)
     pcall(telescope.load_extension, "fzf")
 
-    -- keymaps
-    vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+    -- 🔑 KEYMAPS
+
+    -- files + directories
+    vim.keymap.set("n", "<leader>ff", function()
+      builtin.find_files({
+        find_command = { "fd", "--type", "f", "--type", "d" },
+      })
+    end, { desc = "Find files and directories" })
+
+    -- search text
     vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+
+    -- buffers
     vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
+
+    -- help
     vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help" })
+
+    -- recent files
     vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "Recent files" })
+
+    -- search inside current file
     vim.keymap.set("n", "<leader>fs", builtin.current_buffer_fuzzy_find, { desc = "Search in file" })
   end,
 }
