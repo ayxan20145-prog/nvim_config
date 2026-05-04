8e0dbbc069c64b2aba9a892e26993450c5e208bc
ayxan <ayxan.20145@gmail.com>
2026-05-04 11:53:28 +0400

    nvim10

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -1,5 +1,5 @@
 vim.opt.number = true
-vim.opt.relativenumber = false
+vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
@@ -14,5 +14,3 @@ vim.opt.cursorline = true
 vim.opt.signcolumn = "yes"
 
 vim.g.mapleader = " "
-
-vim.opt.guicursor = "n-v-c:block,i-ci-ve:block,r-cr:hor20,o:hor50"
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -4,10 +4,16 @@ return {
   config = function()
     local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+    -- Rust LSP
     vim.lsp.config("rust_analyzer", {
       capabilities = capabilities,
     })
-
     vim.lsp.enable("rust_analyzer")
+
+    -- C / C++ LSP (clangd)
+    vim.lsp.config("clangd", {
+      capabilities = capabilities,
+    })
+    vim.lsp.enable("clangd")
   end
 }
